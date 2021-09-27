5117f0bb6a2fc4ae406788640c1758096e0ef8cd
yagompassos <yagompassos@gmail.com>
2021-09-26 22:25:48 -0300

    reajustando pastas, improve na main

---
+++ TPs Antigos/TP3/TrabalhoPratico/src/pacote/Cliente.java
@@ -0,0 +1,38 @@
+package pacote;
+
+public class Cliente extends Pessoa  {
+	private int fidelidade;
+
+	Cliente(String n, String c, int i, Unidade u, String e, int f){
+		nome = n;
+		cpf = c;
+		idade = i;
+		unidade = u;
+		email = e;
+		fidelidade = f;
+	}
+	
+	public String toString() {
+		return nome;
+	}
+	
+	public int getFidelidade() {
+		return fidelidade;
+	}
+	public void setFidelidade(int fidelidade) {
+		this.fidelidade = fidelidade;
+	}
+	
+	
+	public String retornaMensalidade(Cliente c) {
+		String custo="";
+			
+		int i = c.getFidelidade();
+		
+		if(i==0) {
+			custo = "60.00";
+		}
+		
+		return custo;
+	}
+}
