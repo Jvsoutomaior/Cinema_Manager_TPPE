4c5c1c6dc5245bdcd86ca550f892a105a9f9124f
João Vítor Motta Souto Maior <soutomaior950@gmail.com>
2025-06-06 00:04:23 -0300

    feat(api): filme update and delete

--- app/routers/filme.py
+++ app/routers/filme.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from ..models.filme import Filme as filmeModel
-from ..schemas.filme import Filme, FilmeCreate
+from ..schemas.filme import Filme, FilmeCreate, FilmeUpdate
 from ..database import get_db
 
 router = APIRouter()
@@ -22,32 +22,31 @@ def read_filme(filme_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Filme not found")
     return db_filme
 
-# @router.get("/", response_model=List[schemas.Filme])
-# def read_filmes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     filmes = db.query(models.Filme).offset(skip).limit(limit).all()
-#     return filmes
+@router.get("/", response_model=List[Filme])
+def read_filmes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    filmes = db.query(filmeModel).offset(skip).limit(limit).all()
+    return filmes
 
 
-
-# @router.put("/{filme_id}", response_model=schemas.Filme)
-# def update_filme(filme_id: int, filme: schemas.FilmeUpdate, db: Session = Depends(get_db)):
-#     db_filme = db.query(models.Filme).filter(models.Filme.id == filme_id).first()
-#     if db_filme is None:
-#         raise HTTPException(status_code=404, detail="Filme not found")
+@router.put("/{filme_id}", response_model=Filme)
+def update_filme(filme_id: int, filme: FilmeUpdate, db: Session = Depends(get_db)):
+    db_filme = db.query(filmeModel).filter(filmeModel.id == filme_id).first()
+    if db_filme is None:
+        raise HTTPException(status_code=404, detail="Filme not found")
     
-#     for key, value in filme.model_dump(exclude_unset=True).items():
-#         setattr(db_filme, key, value)
+    for key, value in filme.model_dump(exclude_unset=True).items():
+        setattr(db_filme, key, value)
     
-#     db.commit()
-#     db.refresh(db_filme)
-#     return db_filme
+    db.commit()
+    db.refresh(db_filme)
+    return db_filme
 
-# @router.delete("/{filme_id}")
-# def delete_filme(filme_id: int, db: Session = Depends(get_db)):
-#     db_filme = db.query(models.Filme).filter(models.Filme.id == filme_id).first()
-#     if db_filme is None:
-#         raise HTTPException(status_code=404, detail="Filme not found")
+@router.delete("/{filme_id}")
+def delete_filme(filme_id: int, db: Session = Depends(get_db)):
+    db_filme = db.query(filmeModel).filter(filmeModel.id == filme_id).first()
+    if db_filme is None:
+        raise HTTPException(status_code=404, detail="Filme not found")
     
-#     db.delete(db_filme)
-#     db.commit()
-#     return {"message": "Filme deleted successfully"} 
+    db.delete(db_filme)
+    db.commit()
+    return {"message": "Filme deleted successfully"} 
