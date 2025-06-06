7a864048cc6112b06cd2acf2b14c47137d787727
João Vítor Motta Souto Maior <soutomaior950@gmail.com>
2025-06-06 00:04:04 -0300

    fix(tests): diferent files for each class

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+from fastapi.testclient import TestClient
+from app.main import app
+import pytest
+from sqlalchemy.orm import Session
+from app.database import SessionLocal, Base, engine
+from app.models.filme import Filme
+
+client = TestClient(app)
+
+@pytest.fixture(scope="function")
+def db():
+    # Create all tables
+    Base.metadata.create_all(bind=engine)
+    
+    # Create a new database session for the test
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def drop_all_tables():
+    # Drop all tables in the database
+    Base.metadata.drop_all(bind=engine)
